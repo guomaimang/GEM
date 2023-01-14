59408a8caa12ec2dd04c256546964a4742c40a43
hanjiaming <47519540+guomaimang@users.noreply.github.com>
2023-01-14 20:26:15 +0800

    update

---
+++ docs/.vuepress/navbar.ts
@@ -0,0 +1,11 @@
+import { navbar } from "vuepress-theme-hope";
+
+export default navbar([
+  "/",
+  { text: "IDEA", icon: "creative", link: "#" },
+  { text: "小组成员", icon: "people", link: "/task/people.html" },
+  { text: "项目解读", icon: "alias", link: "/task/" },
+  { text: "文档修订", icon: "edit", link: "/faq/edit.html" },
+
+
+]);
